7a66e74f16e961f97dc51e68a92515b760ff1a34
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-12 20:12:13 +0200

    made layout fixed and not dynamic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,88 +1,96 @@
-import type { NextPage } from 'next'
-import dynamic from "next/dynamic"
+import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+import Layout from "@components/Layout";
 
 const Home: NextPage = () => {
-  const Layout = dynamic(() => import('@components/Layout'), {ssr:false})
-  const Profile = dynamic(() => import('@components/Profile'), {ssr: false})
+  const Profile = dynamic(() => import("@components/Profile"), { ssr: false });
   return (
     <Layout currentPage="Home">
       <Profile />
-      <p className="default-text mt-8">Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
-      vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu erat
-      vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
-      velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
-      Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
-      Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
-      eget, ullamcorper eu, dui. Quisque dignissim consequat nisl. Pellentesque porta
-      augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat.
-      Suspendisse nulla erat, lacinia nec, pretium vitae, feugiat ac, quam. Etiam sed
-      tellus vel est ultrices. Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
-      vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu erat
-      vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
-      velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
-      Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
-      Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
-      eget, ullamcorper eu, dui. Quisque dignissim consequat nisl. Pellentesque porta
-      augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat.
-      Suspendisse nulla erat, lacinia nec, pretium vitae, feugiat ac, quam. Etiam sed
-      tellus vel est ultrices.Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
-      vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu erat
-      vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
-      velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
-      Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
-      Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
-      eget, ullamcorper eu, dui. Quisque dignissim consequat nisl. Pellentesque porta
-      augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat.
-      Suspendisse nulla erat, lacinia nec, pretium vitae, feugiat ac, quam. Etiam sed
-      tellus vel est ultrices. Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
-      vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu erat
-      vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
-      velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
-      Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
-      Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
-      eget, ullamcorper eu, dui. Quisque dignissim consequat nisl. Pellentesque porta
-      augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat.
-      Suspendisse nulla erat, lacinia nec, pretium vitae, feugiat ac, quam. Etiam sed
-      tellus vel est ultrices.Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
-      vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu erat
-      vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
-      velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
-      Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
-      Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
-      eget, ullamcorper eu, dui. Quisque dignissim consequat nisl. Pellentesque porta
-      augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat.
-      Suspendisse nulla erat, lacinia nec, pretium vitae, feugiat ac, quam. Etiam sed
-      tellus vel est ultrices. Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
-      vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu erat
-      vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
-      velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
-      Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
-      Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
-      eget, ullamcorper eu, dui. Quisque dignissim consequat nisl. Pellentesque porta
-      augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat.
-      Suspendisse nulla erat, lacinia nec, pretium vitae, feugiat ac, quam. Etiam sed
-      tellus vel est ultrices.Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
-      vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu erat
-      vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
-      velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
-      Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
-      Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
-      eget, ullamcorper eu, dui. Quisque dignissim consequat nisl. Pellentesque porta
-      augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat.
-      Suspendisse nulla erat, lacinia nec, pretium vitae, feugiat ac, quam. Etiam sed
-      tellus vel est ultrices. Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
-      vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu erat
-      vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
-      velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
-      Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
-      Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
-      eget, ullamcorper eu, dui. Quisque dignissim consequat nisl. Pellentesque porta
-      augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat.
-      Suspendisse nulla erat, lacinia nec, pretium vitae, feugiat ac, quam. Etiam sed
-      tellus vel est ultrices.
+      <p className="default-text mt-8">
+        Aliquam suscipit. Duis leo est, interdum nec, varius in, facilisis
+        vitae, odio. Phasellus eget leo at urna adipiscing vulputate. Nam eu
+        erat vel arcu tristique mattis. Nullam placerat lorem non augue. Cras et
+        velit. Morbi sapien nulla, volutpat a, tristique eu, molestie ac, felis.
+        Suspendisse sit amet tellus non odio porta pellentesque. Nulla facilisi.
+        Integer iaculis condimentum augue. Nullam urna nulla, vestibulum quis,
+        lacinia eget, ullamcorper eu, dui. Quisque dignissim consequat nisl.
+        Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis
+        pellentesque consequat. Suspendisse nulla erat, lacinia nec, pretium
+        vitae, feugiat ac, quam. Etiam sed tellus vel est ultrices. Aliquam
+        suscipit. Duis leo est, interdum nec, varius in, facilisis vitae, odio.
+        Phasellus eget leo at urna adipiscing vulputate. Nam eu erat vel arcu
+        tristique mattis. Nullam placerat lorem non augue. Cras et velit. Morbi
+        sapien nulla, volutpat a, tristique eu, molestie ac, felis. Suspendisse
+        sit amet tellus non odio porta pellentesque. Nulla facilisi. Integer
+        iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
+        eget, ullamcorper eu, dui. Quisque dignissim consequat nisl.
+        Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis
+        pellentesque consequat. Suspendisse nulla erat, lacinia nec, pretium
+        vitae, feugiat ac, quam. Etiam sed tellus vel est ultrices.Aliquam
+        suscipit. Duis leo est, interdum nec, varius in, facilisis vitae, odio.
+        Phasellus eget leo at urna adipiscing vulputate. Nam eu erat vel arcu
+        tristique mattis. Nullam placerat lorem non augue. Cras et velit. Morbi
+        sapien nulla, volutpat a, tristique eu, molestie ac, felis. Suspendisse
+        sit amet tellus non odio porta pellentesque. Nulla facilisi. Integer
+        iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
+        eget, ullamcorper eu, dui. Quisque dignissim consequat nisl.
+        Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis
+        pellentesque consequat. Suspendisse nulla erat, lacinia nec, pretium
+        vitae, feugiat ac, quam. Etiam sed tellus vel est ultrices. Aliquam
+        suscipit. Duis leo est, interdum nec, varius in, facilisis vitae, odio.
+        Phasellus eget leo at urna adipiscing vulputate. Nam eu erat vel arcu
+        tristique mattis. Nullam placerat lorem non augue. Cras et velit. Morbi
+        sapien nulla, volutpat a, tristique eu, molestie ac, felis. Suspendisse
+        sit amet tellus non odio porta pellentesque. Nulla facilisi. Integer
+        iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
+        eget, ullamcorper eu, dui. Quisque dignissim consequat nisl.
+        Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis
+        pellentesque consequat. Suspendisse nulla erat, lacinia nec, pretium
+        vitae, feugiat ac, quam. Etiam sed tellus vel est ultrices.Aliquam
+        suscipit. Duis leo est, interdum nec, varius in, facilisis vitae, odio.
+        Phasellus eget leo at urna adipiscing vulputate. Nam eu erat vel arcu
+        tristique mattis. Nullam placerat lorem non augue. Cras et velit. Morbi
+        sapien nulla, volutpat a, tristique eu, molestie ac, felis. Suspendisse
+        sit amet tellus non odio porta pellentesque. Nulla facilisi. Integer
+        iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
+        eget, ullamcorper eu, dui. Quisque dignissim consequat nisl.
+        Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis
+        pellentesque consequat. Suspendisse nulla erat, lacinia nec, pretium
+        vitae, feugiat ac, quam. Etiam sed tellus vel est ultrices. Aliquam
+        suscipit. Duis leo est, interdum nec, varius in, facilisis vitae, odio.
+        Phasellus eget leo at urna adipiscing vulputate. Nam eu erat vel arcu
+        tristique mattis. Nullam placerat lorem non augue. Cras et velit. Morbi
+        sapien nulla, volutpat a, tristique eu, molestie ac, felis. Suspendisse
+        sit amet tellus non odio porta pellentesque. Nulla facilisi. Integer
+        iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
+        eget, ullamcorper eu, dui. Quisque dignissim consequat nisl.
+        Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis
+        pellentesque consequat. Suspendisse nulla erat, lacinia nec, pretium
+        vitae, feugiat ac, quam. Etiam sed tellus vel est ultrices.Aliquam
+        suscipit. Duis leo est, interdum nec, varius in, facilisis vitae, odio.
+        Phasellus eget leo at urna adipiscing vulputate. Nam eu erat vel arcu
+        tristique mattis. Nullam placerat lorem non augue. Cras et velit. Morbi
+        sapien nulla, volutpat a, tristique eu, molestie ac, felis. Suspendisse
+        sit amet tellus non odio porta pellentesque. Nulla facilisi. Integer
+        iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
+        eget, ullamcorper eu, dui. Quisque dignissim consequat nisl.
+        Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis
+        pellentesque consequat. Suspendisse nulla erat, lacinia nec, pretium
+        vitae, feugiat ac, quam. Etiam sed tellus vel est ultrices. Aliquam
+        suscipit. Duis leo est, interdum nec, varius in, facilisis vitae, odio.
+        Phasellus eget leo at urna adipiscing vulputate. Nam eu erat vel arcu
+        tristique mattis. Nullam placerat lorem non augue. Cras et velit. Morbi
+        sapien nulla, volutpat a, tristique eu, molestie ac, felis. Suspendisse
+        sit amet tellus non odio porta pellentesque. Nulla facilisi. Integer
+        iaculis condimentum augue. Nullam urna nulla, vestibulum quis, lacinia
+        eget, ullamcorper eu, dui. Quisque dignissim consequat nisl.
+        Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis
+        pellentesque consequat. Suspendisse nulla erat, lacinia nec, pretium
+        vitae, feugiat ac, quam. Etiam sed tellus vel est ultrices.
       </p>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
