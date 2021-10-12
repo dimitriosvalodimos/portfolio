0790cbc091279e06fd7f9ade2371434869ea582f
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-12 15:55:51 +0200

    remade profile comp

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -8,16 +8,19 @@ const Profile = () => {
     const birthDay = new Date('1998-03-05T00:01:00')
     return today.getFullYear() - birthDay.getFullYear()
   }
+  // <div className="mt-8 sm:mt-0 sm:ml-4">
   return (
-    <section className="mt-8">
-      <Image src={profilePic} width={256} height={256} placeholder="blur" className="rounded-full"></Image>
-      <div className="mt-8">
-        <p className="font-semibold text-xl default-text">About me:</p>
-        <hr className="w-2/3 my-1"/>
-        <ul className="default-text list-disc list-inside">
+    <section className="mt-8 flex flex-col sm:flex-row sm:justify-center sm:items-center text-center">
+      <div>
+        <Image src={profilePic} width={256} height={256} placeholder="blur" className="rounded-full"></Image>
+      </div>
+      <div className="sm:ml-8">
+        <p className="font-semibold text-xl default-text mt-8 sm:mt-0">About me:</p>
+        <hr className="my-4"/>
+        <ul className="text-left list-disc list-inside">
           <li>{getAge()} years old</li>
-          <li>studying informatikcs at <Link href="https://www.inf.uni-hamburg.de/en/studies/bachelor/inf.html"><a>UHH</a></Link></li>
-          <li>React(Next.js), Python</li>
+          <li>B.Sc Informatics at <Link href="https://www.inf.uni-hamburg.de/en/studies/bachelor/inf.html"><a>UHH</a></Link></li>
+          <li>Next.js, Python, Java</li>
         </ul>
       </div>
     </section>
