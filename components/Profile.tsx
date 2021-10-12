a20c8450a8f830e661cf76d99eb10740bc419eed
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-12 16:35:26 +0200

    added better spacing for profile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -8,18 +8,17 @@ const Profile = () => {
     const birthDay = new Date('1998-03-05T00:01:00')
     return today.getFullYear() - birthDay.getFullYear()
   }
-  // <div className="mt-8 sm:mt-0 sm:ml-4">
   return (
     <section className="mt-8 flex flex-col sm:flex-row sm:justify-center sm:items-center text-center">
       <div>
         <Image src={profilePic} width={256} height={256} placeholder="blur" className="rounded-full"></Image>
       </div>
-      <div className="sm:ml-8">
+      <div className="sm:ml-8 md:ml-32">
         <p className="font-semibold text-xl default-text mt-8 sm:mt-0">About me:</p>
-        <hr className="my-4"/>
-        <ul className="text-left list-disc list-inside">
+        <hr className="my-4 w-3/4 mx-auto"/>
+        <ul className="text-left leading-relaxed w-3/4 md:w-full mx-auto">
           <li>{getAge()} years old</li>
-          <li>B.Sc Informatics at <Link href="https://www.inf.uni-hamburg.de/en/studies/bachelor/inf.html"><a>UHH</a></Link></li>
+          <li>B.Sc Informatics at <Link href="https://www.inf.uni-hamburg.de/en/studies/bachelor/inf.html"><a className="text-blue-500 underline dark:text-yellow-400" target="_blank" rel="noopener noreferrer">UHH</a></Link></li>
           <li>Next.js, Python, Java</li>
         </ul>
       </div>
