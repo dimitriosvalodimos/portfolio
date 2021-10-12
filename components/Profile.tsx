53e1bfa5bb0ba93c4f7019053bb196f72f0cc99b
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-12 15:08:54 +0200

    added width and height to Image with blurring on loading

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import profilePic from "../public/images/mountain.jpg";
+import Link from "next/link"
 
 const Profile = () => {
   const getAge = () => {
@@ -9,14 +10,14 @@ const Profile = () => {
   }
   return (
     <section className="mt-8">
-      <Image src={profilePic} layout="responsive" className="rounded-full"></Image>
+      <Image src={profilePic} width={256} height={256} placeholder="blur" className="rounded-full"></Image>
       <div className="mt-8">
         <p className="font-semibold text-xl default-text">About me:</p>
-        <hr />
-        <ul className="default-text">
+        <hr className="w-2/3 my-1"/>
+        <ul className="default-text list-disc list-inside">
           <li>{getAge()} years old</li>
-          <li>studying computer science at UHH</li>
-          <li>frequently use React(Next.js), Python</li>
+          <li>studying informatikcs at <Link href="https://www.inf.uni-hamburg.de/en/studies/bachelor/inf.html"><a>UHH</a></Link></li>
+          <li>React(Next.js), Python</li>
         </ul>
       </div>
     </section>
