385d090a4f8085a328d98cedbc5eb7e7b6216357
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-14 21:42:26 +0200

    forgot to remove layout keyword

--- components/TodosLocalStorageModal.tsx
+++ components/TodosLocalStorageModal.tsx
@@ -6,7 +6,6 @@ const TodosLocalStorageModal = ({ close }: TodoModalProps) => {
   const { changeAccess } = useLocalStorage();
   return (
     <motion.div
-      layout
       initial={{ opacity: 0.5, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, x: "100%" }}
