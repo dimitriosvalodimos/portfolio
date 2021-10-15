8cff0a510c57f17a1cdfb1081e39b1cafc6e0e6d
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-15 18:11:19 +0200

    disable prefetch for Navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,17 +13,17 @@ const Navbar = () => {
     <nav className="flex justify-between items-center sticky top-0 bg-gray-50 z-50 dark:bg-gray-700 h-16 border-b border-gray-800 dark:border-gray-50">
       <ul className="flex justify-evenly items-center w-full h-full">
         <li className="font-semibold text-lg default-text px-2 py-3 hoverable default-transition">
-          <Link href="/">
+          <Link href="/" prefetch={false}>
             <a className="px-2 py-3">Home</a>
           </Link>
         </li>
         <li className="font-semibold text-lg default-text px-2 py-3 hoverable default-transition">
-          <Link href="/projects">
+          <Link href="/projects" prefetch={false}>
             <a className="px-2 py-3">Projects</a>
           </Link>
         </li>
         <li className="font-semibold text-lg default-text px-2 py-3 hoverable default-transition">
-          <Link href="/contact">
+          <Link href="/contact" prefetch={false}>
             <a className="px-2 py-3">Contact</a>
           </Link>
         </li>
