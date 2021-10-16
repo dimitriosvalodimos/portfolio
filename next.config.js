2beb1833dfc14c5bdc65d9b8ab08bf8a562cc994
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-16 20:26:10 +0200

    removed dotnev

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const webpack = require('webpack');
-const { parsed: myEnv } = require('dotenv').config();
 
 module.exports = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
-  }
+  reactStrictMode: true
 };
