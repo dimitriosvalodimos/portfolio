a00decea5d4feff92ef70ce888e40ac2924da7d4
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-16 20:17:40 +0200

    changed dotenv webpack setup

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const webpack = require("webpack");
-const { parsed: myEnv } = require("dotenv").config({ path: "./.env" });
+const webpack = require('webpack');
+const { parsed: myEnv } = require('dotenv').config();
 
 module.exports = {
   reactStrictMode: true,
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
     return config;
-  },
+  }
 };
