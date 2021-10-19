e5b59785372aaea348d300a557baddca4418fd8e
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-19 11:34:43 +0200

    added lang=de to Html tag, to make word-breaks and grammar errors display correctly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,28 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html>
+      <Html lang="de">
         <Head />
         <body className="bg-gray-50 dark:bg-gray-700 default-text transition duration-200">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
