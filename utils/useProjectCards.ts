09884f4dc54efd1bc75ac398612965d402b85165
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2021-10-19 11:41:26 +0200

    switched to german

--- utils/useProjectCards.ts
+++ utils/useProjectCards.ts
@@ -1,51 +1,52 @@
-import create from "zustand";
-import { CardData, CardStore } from "@utils/commonProps";
+import create from 'zustand';
+import { CardData, CardStore } from '@utils/commonProps';
 
 const useCards = create<CardStore>((set, get) => ({
   cardData: [
     {
-      name: "Portfolio",
+      name: 'Portfolio',
       description:
-        "The site your on right now! Since you're already here I'll spare you the link to the demo and give you the link to the GitHub repository.",
-      category: "Web",
-      linkTo: "https://github.com/dimitriosvalodimos/portfolio",
+        'Die Seite auf der du gerade bist! Statt dir also einen Demo-Link unten bereitzustellen, verlinke ich dir das Projekt auf GitHub.',
+      category: 'Web',
+      linkTo: 'https://github.com/dimitriosvalodimos/portfolio',
       stack: [
-        "Next.js",
-        "TailwindCSS",
-        "Typescript",
-        "zustand",
-        "framer-motion",
-      ],
+        'Next.js',
+        'TailwindCSS',
+        'Typescript',
+        'zustand',
+        'framer-motion'
+      ]
     },
     {
-      name: "Todos",
+      name: 'ToDos',
       description:
-        "Simple Todo App with saving to localStorage and using Zustand for very easy access to the data.",
-      category: "Web",
-      linkTo: "/todos",
+        'Eine einfache ToDo-App, die deine Einträge auch im localStorage speichern kann.',
+      category: 'Web',
+      linkTo: '/todos',
       stack: [
-        "Next.js",
-        "TailwindCSS",
-        "Typescript",
-        "zustand",
-        "framer-motion",
-      ],
+        'Next.js',
+        'TailwindCSS',
+        'Typescript',
+        'zustand',
+        'framer-motion'
+      ]
     },
     {
-      name: "Weather",
-      description: "A simple weather app using Micro Weather API.",
-      category: "Web",
-      linkTo: "/weather",
+      name: 'Wetter',
+      description:
+        'Kleine Wetter-App, um die wichtigsten Eckdaten zum jetzigen Wetter zu finden.',
+      category: 'Web',
+      linkTo: '/weather',
       stack: [
-        "Next.js",
-        "TailwindCSS",
-        "Typescript",
-        "zustand",
-        "framer-motion",
-      ],
-    },
+        'Next.js',
+        'TailwindCSS',
+        'Typescript',
+        'zustand',
+        'framer-motion'
+      ]
+    }
   ],
-  getCardAt: (index: number) => get().cardData[index],
+  getCardAt: (index: number) => get().cardData[index]
 }));
 
 export { useCards };
